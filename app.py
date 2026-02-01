41596ddf0c838c7b03673a5c89ecf0ffa20141d5
madan1440 <madan2503@gmail.com>
2026-02-01 16:58:24 +0530

    Modify app.run to use default parameters
    Comment out the app.run line and use default parameters.

--- app.py
+++ app.py
@@ -395,4 +395,5 @@ def download_emi():
 
 # -------- run --------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=DEBUG)
+    # app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=DEBUG)
+    app.run()
